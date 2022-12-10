ca5f08836eb569854874abd5f3dc83b28c9d02fb
verboczy <verbocika@gmail.com>
2022-12-10 13:23:33 +0100

    Create Spring Boot project - initialize project #5

---
+++ spending-tracker-backend/src/main/java/hu/verboczy/spendingtrackerbackend/SpendingTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.verboczy.spendingtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ spending-tracker-backend/src/test/java/hu/verboczy/spendingtrackerbackend/SpendingTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.verboczy.spendingtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
